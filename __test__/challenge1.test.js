0fc8290fae6eff9a07979ccf64cb8348f51c2422
Juan Ceballos <jacu29@gmail.com>
2021-09-01 19:58:51 -0500

    Agreagr nueva caracteristica al proyecto

--- __test__/challenge1.test.js
+++ __test__/challenge1.test.js
@@ -1,4 +1,4 @@
-import {suma} from '../index.js'
+import {suma, resta} from '../index.js'
 
 test("Debería regresar 8 al pasar como argumento 6 y 2", () => {
     let a = 6;
@@ -7,4 +7,13 @@ test("Debería regresar 8 al pasar como argumento 6 y 2", () => {
     let result = suma(a,b);
 
     expect(result).toBe(8);
+})
+
+test("Debería regresar 4 al pasar como argumentos 8 y 4", () => {
+    let a = 8;
+    let b = 4;
+
+    let result = resta(a,b);
+
+    expect(result).toBe(4);
 })
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const suma = (a, b) => {
     return a + b;
 }
+
+export const resta = (a, b) => {
+    return a*b
+}
